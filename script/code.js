bb1182a3f5bbfdd31e6368ad872ce1779f5a9807
S1bu <Sibusisodyan@gmail.com>
2023-05-17 14:19:26 +0200

    done with day 4 objacts and arrays

---
+++ script/code.js
@@ -0,0 +1,33 @@
+// 1.
+let person = {
+    name:'Sibusiso',
+    surname:'Dyan',
+    codingLan:'Javascript'
+};
+
+// 2.
+let person2 = {
+    name:'Sibusiso',
+    surname:'Dyan',
+    codingLan:'Javascript'
+};
+
+// 3.
+console.log(person)
+console.log(person2)
+
+// 4.
+let array = [
+     person = {
+        name:'Sibusiso',
+        surname:'Dyan',
+        codingLan:'Javascript'
+    },
+     person2 = {
+        name:'Sibusiso',
+        surname:'Dyan',
+        codingLan:'Javascript'
+    }
+]
+
+console.log(array)
